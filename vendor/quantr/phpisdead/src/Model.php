c0dd324c89159d6127383f726467ec936262a6e1
Peter <peter@quantr.hk>
2024-11-20 18:31:54 +0800

    dev

--- vendor/quantr/phpisdead/src/Model.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Quantr\PhpIsDead;
-
-class Model{
-	public function func1(){
-		echo "Php is dead, peter 2";
-	}
-}
--- vendor/quantr/phpisdead/src/Request.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace Quantr\PhpIsDead;
-
-class Request {}
